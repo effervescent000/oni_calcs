36f90ea4f1c78fc7c3244a78ba75e5f9e19a1ca0
effervescent000 <kislio01@gmail.com>
2022-02-03 11:01:41 -0500

    added login checking on load

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import "./styles/main.scss";
 
 import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import axios from "axios";
+import Cookies from "js-cookie";
 
 import Header from "./components/header";
 import MainPage from "./components/main-page";
@@ -16,6 +18,24 @@ function App() {
     const [loggedIn, setLoggedIn] = useState(false);
     const [profile, setProfile] = useState({});
     const [user, setUser] = useState({});
+
+    useEffect(() => {
+        if (!loggedIn) {
+            axios
+                .get(`${process.env.REACT_APP_DOMAIN}/auth/check`, {
+                    withCredentials: true,
+                    headers: { "X-CSRF-TOKEN": Cookies.get("csrf_access_token") },
+                })
+                .then((response) => {
+                    console.log(response);
+                    if (Object.keys(response.data).length > 0) {
+                        toggleLogIn();
+                        setUser(response.data);
+                    }
+                })
+                .catch((error) => console.log(error.response));
+        }
+    });
 
     const toggleLogIn = () => {
         if (loggedIn) {
