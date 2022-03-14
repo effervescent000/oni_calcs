0c2bbc8f6c6dba334fe58416e69c257be71709b3
effervescent000 <kislio01@gmail.com>
2022-03-14 08:55:39 -0400

    disabling reaflow stuff

--- src/components/food-calc/food-graph.js
+++ src/components/food-calc/food-graph.js
@@ -1,16 +1,12 @@
 import React from "react";
-import { Canvas } from "reaflow";
+// import { Canvas } from "reaflow";
 
 import foods from "../../foods";
 
 const FoodGraph = (props) => {
-    const { nodes, edges } = props.graphData;
+    // const { nodes, edges } = props.graphData;
 
-    return (
-        <div id="graph-wrapper">
-            <Canvas nodes={nodes} />
-        </div>
-    );
+    return <div id="graph-wrapper">{/* <Canvas nodes={nodes} /> */}</div>;
 };
 
 export default FoodGraph;
